a503b716e38c458ff2657945070a3e7722079f92
Ruize Yu <ry375@brg-zhang-xcel.ece.cornell.edu>
2026-03-23 20:14:36 -0400

    add top design

--- systolic/test_tile.py
+++ systolic/test_tile.py
@@ -5,7 +5,8 @@ import tempfile
 
 import pytest
 import allo
-from allo.ir.types import int8, Stream, UInt, float32, int32
+import scipy.special
+from allo.ir.types import int8, Stream, UInt, float32, int32, index
 from allo.utils import get_np_struct_type
 import allo.dataflow as df
 import allo.backend.hls as hls
@@ -17,14 +18,17 @@ P0, P1 = HEAD_DIM, BLOCK_T
 D = 1.0 / float(HEAD_DIM ** 0.5)
 
 @df.region()
-def top(
+def compute_block_attention(
     Q: float32[BLOCK_T, HEAD_DIM],
+    Max_in: float32[BLOCK_T],
+    S_in: float32[BLOCK_T, HEAD_DIM],
+    Sum_in: float32[BLOCK_T],
     K_m: float32[BLOCK_T, HEAD_DIM],
     V: float32[BLOCK_T, HEAD_DIM],
+    S_out: float32[BLOCK_T, HEAD_DIM],
+    Sum_out: float32[BLOCK_T],
+    Max_out: float32[BLOCK_T],
     is_first_block: int32[1],
-    Sum: float32[BLOCK_T],
-    Max: float32[BLOCK_T],
-    S: float32[BLOCK_T, HEAD_DIM],
 ):
     L_Q: Stream[float32, 1024][P1]
     L_K: Stream[float32, 1024][P0]
@@ -37,18 +41,18 @@ def top(
     fifo_SD: Stream[float32, 1024][P0, P1]
 
 #load Q matrix
-    @df.kernel(mapping=[1], args=[Q, Max, S, Sum])
-    def offchip_loadQ(local_Q: float32[BLOCK_T, HEAD_DIM], local_Max: float32[BLOCK_T], local_S: float32[BLOCK_T, HEAD_DIM], local_Sum: float32[BLOCK_T]):
-
+    @df.kernel(mapping=[1], args=[Q, Max_in, S_in, Sum_in])
+    def offchip_loadQ(local_Q: float32[BLOCK_T, HEAD_DIM], local_Max_in: float32[BLOCK_T], local_S_in: float32[BLOCK_T, HEAD_DIM], local_Sum_in: float32[BLOCK_T]):
+        
         for i in range(BLOCK_T):
-            val = local_Sum[i]
+            val = local_Sum_in[i]
             if i == 0: L_Q[0].put(val)
             elif i == 1: L_Q[1].put(val)
             elif i == 2: L_Q[2].put(val)
             elif i == 3: L_Q[3].put(val)
 
         for i in range(BLOCK_T):
-            val = local_Max[i]
+            val = local_Max_in[i]
             if i == 0: L_Q[0].put(val)
             elif i == 1: L_Q[1].put(val)
             elif i == 2: L_Q[2].put(val)
@@ -62,7 +66,7 @@ def top(
             elif i == 3: L_Q[3].put(val)
 
         for i, j in dsl.grid(BLOCK_T, HEAD_DIM):
-            val = local_S[i, j]
+            val = local_S_in[i, j]
             if i == 0: L_Q[0].put(val)
             elif i == 1: L_Q[1].put(val)
             elif i == 2: L_Q[2].put(val)
@@ -89,26 +93,26 @@ def top(
 
 
 #store the rowsum and output O matrix
-    @df.kernel(mapping=[1], args=[S, Sum, Max])
-    def offchip_storeS(local_S: float32[BLOCK_T, HEAD_DIM], local_Sum: float32[BLOCK_T], local_Max: float32[BLOCK_T]):
+    @df.kernel(mapping=[1], args=[S_out, Sum_out, Max_out])
+    def offchip_storeS(local_S_out: float32[BLOCK_T, HEAD_DIM], local_Sum_out: float32[BLOCK_T], local_Max_out: float32[BLOCK_T]):
         
         for i in range(BLOCK_T):
-            if i == 0: local_Max[0] = L_S[0].get()
-            elif i == 1: local_Max[1] = L_S[1].get()
-            elif i == 2: local_Max[2] = L_S[2].get()
-            elif i == 3: local_Max[3] = L_S[3].get()
+            if i == 0: local_Max_out[0] = L_S[0].get()
+            elif i == 1: local_Max_out[1] = L_S[1].get()
+            elif i == 2: local_Max_out[2] = L_S[2].get()
+            elif i == 3: local_Max_out[3] = L_S[3].get()
 
         for i in range(BLOCK_T):
-            if i == 0: local_Sum[0] = L_S[0].get()
-            elif i == 1: local_Sum[1] = L_S[1].get()
-            elif i == 2: local_Sum[2] = L_S[2].get()
-            elif i == 3: local_Sum[3] = L_S[3].get()
+            if i == 0: local_Sum_out[0] = L_S[0].get()
+            elif i == 1: local_Sum_out[1] = L_S[1].get()
+            elif i == 2: local_Sum_out[2] = L_S[2].get()
+            elif i == 3: local_Sum_out[3] = L_S[3].get()
 
         for m, n in dsl.grid(BLOCK_T, HEAD_DIM):
-            if m == 0: local_S[m, n] = L_S[0].get()
-            elif m == 1: local_S[m, n] = L_S[1].get()
-            elif m == 2: local_S[m, n] = L_S[2].get()
-            elif m == 3: local_S[m, n] = L_S[3].get()
+            if m == 0: local_S_out[m, n] = L_S[0].get()
+            elif m == 1: local_S_out[m, n] = L_S[1].get()
+            elif m == 2: local_S_out[m, n] = L_S[2].get()
+            elif m == 3: local_S_out[m, n] = L_S[3].get()
 
 
 
@@ -116,7 +120,7 @@ def top(
     def gemm(is_first: int32[1]):
         i, j = df.get_pid()
 
-        with allo.meta_if(j == 0 and i == BLOCK_T-1):
+        with allo.meta_if(j == 0 and i == P0-1):
             q: float32
             v: float32
             alpha: float32
@@ -143,8 +147,7 @@ def top(
             s = fifo_SD[i,j].get()
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             if max > pre_m:
                 new_m = max
@@ -178,7 +181,7 @@ def top(
 
 
                 
-        with allo.meta_elif(i == BLOCK_T-1 and j == HEAD_DIM-1):
+        with allo.meta_elif(i == P0-1 and j == P1-1):
             q: float32
             alpha: float32
             beta: float32
@@ -204,8 +207,7 @@ def top(
             s = fifo_SD[i,j].get()
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             if max > pre_m:
                 new_m = max
@@ -238,7 +240,7 @@ def top(
 
 
         #bottom two    
-        with allo.meta_elif(i == BLOCK_T-1):
+        with allo.meta_elif(i == P0-1):
             q: float32
             alpha: float32
             beta: float32
@@ -266,8 +268,7 @@ def top(
             s = fifo_SD[i,j].get()
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             if max > pre_m:
                 new_m = max
@@ -314,15 +315,15 @@ def top(
                 fifo_K[i,j+1].put(a)
                 s = fifo_S[i,j].get()
                 s = s + a * q
+                s = s * D
                 #the top row will act as a comparator and compute the row's max value on the fly
                 if s > max:
                     max = s
                 if BLOCK_T-i-m-1>0:
                     fifo_SD[i+1,j].put(s)
             #minus row max to avoid overflow
-            s = s - max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             fifo_SD[i+1,j].put(s)
             #input V matrix and compute its multiplication with P, the element will stream through systolic array
@@ -344,15 +345,15 @@ def top(
                 a = fifo_K[i,j].get()
                 s = fifo_S[i,j].get()
                 s = s + a * q
+                s = s*D
                 #the top row will act as a comparator and compute the row's max value on the fly
                 if s > max:
                     max = s
                 if BLOCK_T-i-m-1>0:
                     fifo_SD[i+1,j].put(s) #keep one s
             #minus row max to avoid overflow
-            s = s-max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
             
             fifo_SD[i+1,j].put(s)
             #input V matrix and compute its multiplication with P, the element will stream through systolic array
@@ -376,15 +377,15 @@ def top(
                 fifo_K[i,j+1].put(a)
                 s = fifo_S[i,j].get()
                 s = s + a * q
+                s = s*D
                 #the top row will act as a comparator and compute the row's max value on the fly
                 if s > max:
                     max = s
                 if BLOCK_T-i-m-1>0:
                     fifo_SD[i+1,j].put(s)
             #minus row max to avoid overflow
-            s = s - max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             fifo_SD[i+1,j].put(s)
             #input V matrix and compute its multiplication with P, the element will stream through systolic array
@@ -393,7 +394,7 @@ def top(
                 fifo_K[i,j+1].put(v)
                 ss = v * s
                 fifo_SD[i+1,j].put(ss)
-        
+
         #left side 2
         with allo.meta_elif(j == 0):
             q: float32 = 0
@@ -421,9 +422,8 @@ def top(
                     
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             ss: float32 = 0.0
             ss = s + fifo_SD[i,j].get()
@@ -435,9 +435,6 @@ def top(
                 ss = fifo_SD[i,j].get()
                 ss = ss + v * s
                 fifo_SD[i+1,j].put(ss)
-
-
-
 
 
         #right side two
@@ -465,9 +462,8 @@ def top(
                     fifo_SD[i+1, j].put(s)
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             ss: float32 = 0.0
             ss = s + fifo_SD[i,j].get()
@@ -507,9 +503,8 @@ def top(
 
             max = fifo_SD[i,j].get()
             #minus row max to avoid overflow
-            s = s - max
             fifo_SD[i+1,j].put(max)
-            s = allo.exp(s*D)
+            s = allo.exp(s-max)
 
             ss: float32 = 0.0
             ss = s + fifo_SD[i,j].get()
@@ -523,75 +518,135 @@ def top(
                 fifo_SD[i+1,j].put(ss)
 
 
-def test_large_scale_gemm():
+BATCH_SIZE: int = 4
+CONTEXT_LENGTH: int = 16
+HIDDEN_SIZE: int = 64
+NUM_HEADS: int = 16
+BLOCK_T: int = 4
 
-    SEQ_LEN = 8
+HEAD_DIM = HIDDEN_SIZE // NUM_HEADS
+D_SQRT = float(HEAD_DIM ** 0.5)
+THREE_H = 3 * HIDDEN_SIZE
+IN_ELEMS = BATCH_SIZE * CONTEXT_LENGTH * THREE_H
+OUT_ELEMS = BATCH_SIZE * CONTEXT_LENGTH * NUM_HEADS * HEAD_DIM
 
-    Q_full = np.random.uniform(-1.0, 1.0, (SEQ_LEN, HEAD_DIM)).astype(np.float32)
-    K_full = np.random.uniform(-1.0, 1.0, (SEQ_LEN, HEAD_DIM)).astype(np.float32)
-    V_full = np.random.uniform(-1.0, 1.0, (SEQ_LEN, HEAD_DIM)).astype(np.float32)
+def load_tile(global_mem: float32[IN_ELEMS], local_tile: float32[BLOCK_T, HEAD_DIM], b: index, h: index, t_start: index, type_offset: int32):
+    for t, d in allo.grid(BLOCK_T, HEAD_DIM):
+        t_global: index = t_start + t
+        global_c: index = type_offset * HIDDEN_SIZE + h * HEAD_DIM + d
+        idx: index = b * (CONTEXT_LENGTH * THREE_H) + t_global * THREE_H + global_c
+        local_tile[t, d] = global_mem[idx]
 
-    S_full = np.dot(Q_full, K_full.T)
-    row_max = np.max(S_full, axis=1, keepdims=True)
-    P_full = np.exp((S_full - row_max)*D)
-    row_sum = np.sum(P_full, axis=1, keepdims=True)
-    O_golden = np.dot(P_full, V_full) / row_sum
+def store_tile(global_mem: float32[OUT_ELEMS], local_tile: float32[BLOCK_T, HEAD_DIM], b: index, h: index, t_start: index):
+    for t, d in allo.grid(BLOCK_T, HEAD_DIM):
+        t_global: index = t_start + t
+        idx: index = (((b * CONTEXT_LENGTH + t_global) * NUM_HEADS + h) * HEAD_DIM + d)
+        global_mem[idx] = local_tile[t, d]
 
-    Q_tile = Q_full[0:BLOCK_T, :]
-    O_hw_state = np.zeros((BLOCK_T, HEAD_DIM), dtype=np.float32)
-    Sum_hw_state = np.zeros(BLOCK_T, dtype=np.float32)
-    Max_hw_state = np.full(BLOCK_T, -1e30, dtype=np.float32)
+def compute_engine(input_mem: float32[IN_ELEMS], output_mem: float32[OUT_ELEMS]):
+    Q_sram: float32[BLOCK_T, HEAD_DIM]
+    K_sram: float32[BLOCK_T, HEAD_DIM]
+    V_sram: float32[BLOCK_T, HEAD_DIM]
 
-    #sim_mod = df.build(top, target="simulator")
-    #sim_mod(A_hw, B_hw, V_hw, O_hw_emu, Sum_hw_emu)
-    #O_emu_out = O_hw_emu.reshape((M,K))
-    #np.testing.assert_allclose(Sum_hw_emu, Sum_golden, atol=1e-5)
-    #np.testing.assert_allclose(O_emu_out, O_golden, atol=1e-4)
-    #print("Dataflow Simulator Passed!")
+    S_in: float32[BLOCK_T, HEAD_DIM]
+    m_in: float32[BLOCK_T]
+    l_in: float32[BLOCK_T]
+
+    S_out: float32[BLOCK_T, HEAD_DIM]
+    m_out: float32[BLOCK_T]
+    l_out: float32[BLOCK_T]
+
+    is_first_arr: int32[1]
+
+    for b, h in allo.grid(BATCH_SIZE, NUM_HEADS):
+        for tr in range(0, CONTEXT_LENGTH, BLOCK_T):
+            load_tile(input_mem, Q_sram, b, h, tr, 0)
+            for m in range(BLOCK_T):
+                m_in[m] = -1e30
+                l_in[m] = 0.0
+                for n in range(HEAD_DIM):
+                    S_in[m, n] = 0.0
+                    
+            for tc in range(0, CONTEXT_LENGTH, BLOCK_T):
+                load_tile(input_mem, K_sram, b, h, tc, 1)
+                load_tile(input_mem, V_sram, b, h, tc, 2)
+
+                if tc == 0:
+                    is_first_arr[0] = 1
+                else:
+                    is_first_arr[0] = 0
+                compute_block_attention(
+                    Q_sram, m_in, S_in, l_in,
+                    K_sram, V_sram,
+                    S_out, l_out, m_out, is_first_arr
+                )
+
+                for m in range(BLOCK_T):
+                    m_in[m] = m_out[m]
+                    l_in[m] = l_out[m]
+                    for n in range(HEAD_DIM):
+                        S_in[m,n] = S_out[m,n]
+                
+            for i in range(BLOCK_T):
+                inv_l: float32 = 1.0 / (l_out[i] + 1e-9)
+                for d in range(HEAD_DIM):
+                    S_out[i, d] = S_out[i, d] * inv_l
+
+            store_tile(output_mem, S_out, b, h, tr)
+
+A = np.random.rand(IN_ELEMS).astype(np.float32)
+B = np.zeros((OUT_ELEMS), dtype=np.float32)
+
+s1 = allo.customize(load_tile)
+s1.pipeline("d")
+s1.pipeline("t")
+
+s2 = allo.customize(store_tile)
+s2.pipeline("d")
+s2.pipeline("t")
+
+s = allo.customize(compute_engine)
+
+s.compose([s1, s2])
+
+if hls.is_available("vitis_hls"):
+    mod_csyn = s.build(
+        target="vitis_hls", 
+        mode="csyn", 
+        project="flash_attention_csyn",
+        wrap_io=True
+    )
+    mod_csyn()
+
+    mod_hw = s.build(
+        target="vitis_hls", 
+        mode="hw_emu", 
+        project="flash_attention_hw_emu",
+        wrap_io=True
+    )
+    mod_hw(A, B)
 
 
-    if hls.is_available("vitis_hls"):
+A_reshaped = A.reshape((BATCH_SIZE, CONTEXT_LENGTH, 3, NUM_HEADS, HEAD_DIM))
 
-        modc = df.build(
-            top,
-            target="vitis_hls",
-            mode="csyn",
-            project="systolic_csyn",
-            wrap_io=False,
-        )
-        modc()
+Q_np = A_reshaped[:, :, 0, :, :]
+K_np = A_reshaped[:, :, 1, :, :]
+V_np = A_reshaped[:, :, 2, :, :]
 
-        modhw = df.build(
-            top,
-            target="vitis_hls",
-            mode="hw_emu",
-            project="systolic_hw_emu",
-            wrap_io=False,
-        )
+Q_np = Q_np.transpose((0, 2, 1, 3))
+K_np = K_np.transpose((0, 2, 1, 3))
+V_np = V_np.transpose((0, 2, 1, 3))
 
-        for tc in range(0, SEQ_LEN, BLOCK_T):
-            is_first_val = 1 if tc == 0 else 0
-            is_first_block = np.array([is_first_val], dtype=np.int32)
+scores = np.matmul(Q_np, K_np.transpose((0, 1, 3, 2)))
+scores = scores * (1.0 / D_SQRT)
+attn_weights = scipy.special.softmax(scores, axis=-1)
+out_np = np.matmul(attn_weights, V_np)
+out_np_reshaped = out_np.transpose((0, 2, 1, 3)).flatten()
 
-            K_tile = K_full[tc:tc+BLOCK_T, :]
-            V_tile = V_full[tc:tc+BLOCK_T, :]
+np.testing.assert_allclose(B, out_np_reshaped, rtol=1e-4, atol=1e-4)
+print("Passed")
 
-            modhw(
-                Q_tile.flatten(),
-                K_tile.flatten(),
-                V_tile.flatten(),
-                is_first_block,
-                O_hw_state.flatten(),
-                Sum_hw_state.flatten(),
-                Max_hw_state.flatten(),
-            )
 
-        print("Hardware computation finished!!!")
-
-        O_hw_final = O_hw_state / Sum_hw_state.reshape(BLOCK_T, 1)
-
-        np.testing.assert_allclose(O_hw_final, O_golden[0:BLOCK_T, :], atol=1e-3)
-        print("Tiling test pass!!!")
 
 if __name__ == "__main__":
-    test_large_scale_gemm()
+    test_tiled_systolic()
